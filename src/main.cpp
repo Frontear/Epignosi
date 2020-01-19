6970fc3f89c438e7c984309929509b82df48b32c
Ali Rizvi <frontear4594@gmail.com>
2020-01-18 21:23:03 -0500

    Create an incredibly basic implementation
    This will work this time

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
+#include <fstream>
 #include <iostream>
 
+using std::ifstream, std::ios, std::cout, std::endl, std::hex, std::dec;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ifstream file("resources/HelloWorld.class", ios::binary);
+
+    uint8_t buffer[4];
+    for (auto i = 4 - 1; i >= 0; --i) {
+        buffer[i] = file.get();
+    }
+
+    cout << hex << *reinterpret_cast<uint32_t*>(buffer) << dec << endl; // magic
+
     return 0;
 }
