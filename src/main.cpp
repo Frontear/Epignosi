81c27f7d057a4e67b703df31bcf062dc3e610710
Ali Rizvi <frontear4594@gmail.com>
2020-01-18 21:29:25 -0500

    Add more to the example

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,19 @@
 #include <fstream>
 #include <iostream>
 
+#define read(x) uint8_t buffer[x]; for (auto i = x - 1; i >= 0; --i) buffer[i] = file.get()
+#define as_u4 *reinterpret_cast<uint32_t*>(buffer)
+#define as_u2 *reinterpret_cast<uint16_t*>(buffer)
+
 using std::ifstream, std::ios, std::cout, std::endl, std::hex, std::dec;
 
 int main() {
     ifstream file("resources/HelloWorld.class", ios::binary);
 
-    uint8_t buffer[4];
-    for (auto i = 4 - 1; i >= 0; --i) {
-        buffer[i] = file.get();
-    }
-
-    cout << hex << *reinterpret_cast<uint32_t*>(buffer) << dec << endl; // magic
+    { read(4); cout << hex << as_u4 << dec << endl; } // magic
+    { read(2); cout << as_u2 << endl; } // minor_version
+    { read(2); cout << as_u2 << endl; } // major_version
+    { read(2); cout << as_u2 << endl; } // constant_pool_count
 
     return 0;
 }
